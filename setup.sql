41468c296b809f045df32f08857499814925099c
suntorybikkle <suntorybikkle77@gmail.com>
2020-05-06 01:55:57 +0900

    fix: 科目カラムの付け忘れ修正

--- setup.sql
+++ setup.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS records;
 
 CREATE TABLE records (
   id SERIAL PRIMARY KEY NOT NULL,
-  userId INT NOT NULL,
+  user_id INT NOT NULL,
+  subject_id INT,
   study_time INT NOT NULL,
   date_time TIMESTAMP NOT NULL
 );
