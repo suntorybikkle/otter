92b97dda3f1ad5fa8f66f5d76c44a7a88e1c8dd4
suntorybikkle <suntorybikkle77@gmail.com>
2020-05-06 15:12:26 +0900

    add: データ操作ひな形

--- data.go
+++ data.go
@@ -18,7 +18,6 @@ type Record struct {
 var Db *sql.DB
 
 func init() {
-	fmt.Println("init")
 	var err error
 	Db, err = sql.Open("postgres", "user=ldb dbname=ldb password=ldb sslmode=disable")
 	if err != nil {
@@ -26,15 +25,16 @@ func init() {
 	}
 }
 
-func GetRecord(id int) (record Record, err error) {
+func GetLastRecord(id int) (record Record, err error) {
 	record = Record{}
-	err = Db.QueryRow("SELECT id, user_id, subject_id, study_time, date_time FROM records WHERE id = $1", id).Scan(
+	err = Db.QueryRow("SELECT id, user_id, subject_id, study_time, date_time FROM records WHERE user_id = $1 ORDER BY Id DESC LIMIT 1", id).Scan(
 		&record.Id, &record.UserId, &record.SubjectId, &record.StudyTime, &record.DateTime)
 	return
 }
 
 func GetAllRecord(userId int) (records []Record, err error) {
 	rows, err := Db.Query("SELECT id, user_id, subject_id, study_time, date_time FROM records WHERE user_id = $1", userId)
+	defer rows.Close()
 	if err != nil {
 		log.Println(err)
 		return
@@ -48,18 +48,31 @@ func GetAllRecord(userId int) (records []Record, err error) {
 		}
 		records = append(records, record)
 	}
-	rows.Close()
+	return
+}
+
+func (record *Record) Create() (err error) {
+	statement := "INSERT INTO records(user_id, subject_id, study_time, date_time) VALUES($1, $2, $3, $4) RETURNING id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		log.Println(err)
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(record.UserId, record.SubjectId, record.StudyTime, record.DateTime).Scan(&record.Id)
 	return
 }
 
 func main() {
-	fmt.Println("main")
-	record, _ := GetRecord(1)
+	record, _ := GetLastRecord(1)
 	fmt.Println(record)
-	record, _ = GetRecord(2)
 	records, _ := GetAllRecord(1)
 	fmt.Println(records)
-	for _, record := range records {
-		fmt.Println(record)
-	}
+	record = Record{UserId: 1, SubjectId: 1, StudyTime: 1234, DateTime: "2020-09-09 09:09:09"}
+	fmt.Println(record)
+	record.Create()
+	fmt.Println(record)
+	records, _ = GetAllRecord(1)
+	fmt.Println(records)
+	record, _ = GetLastRecord(1)
+	fmt.Println(record)
 }
