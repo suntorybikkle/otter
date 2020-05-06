ad5c6b7345cd125cd66529abded58132b2a87013
suntorybikkle <suntorybikkle77@gmail.com>
2020-05-06 17:52:51 +0900

    fix: テーブル名の変更

--- example.sql
+++ example.sql
@@ -1,7 +1,7 @@
-INSERT INTO records(user_id, subject_id, study_time, date_time) VALUES(1, 1, 300, '2020-05-06 01:02:30');
-INSERT INTO records(user_id, subject_id, study_time, date_time) VALUES(1, 1, 2000, '2020-05-06 01:12:30');
-INSERT INTO records(user_id, subject_id, study_time, date_time) VALUES(1, 2, 10, '2020-05-07 01:02:30');
-INSERT INTO records(user_id, subject_id, study_time, date_time) VALUES(1, 1, 700, '2020-05-12 12:03:30');
-INSERT INTO records(user_id, subject_id, study_time, date_time) VALUES(1, 3, 4300, '2020-05-12 16:12:30');
-INSERT INTO records(user_id, subject_id, study_time, date_time) VALUES(1, 2, 300, '2020-05-12 19:12:30');
-INSERT INTO records(user_id, subject_id, study_time, date_time) VALUES(1, 1, 10, '2020-05-12 23:40:00');
+INSERT INTO study_infos(user_id, subject_id, study_time, date_time) VALUES(1, 1, 300, '2020-05-06 01:02:30');
+INSERT INTO study_infos(user_id, subject_id, study_time, date_time) VALUES(1, 1, 2000, '2020-05-06 01:12:30');
+INSERT INTO study_infos(user_id, subject_id, study_time, date_time) VALUES(1, 2, 10, '2020-05-07 01:02:30');
+INSERT INTO study_infos(user_id, subject_id, study_time, date_time) VALUES(1, 1, 700, '2020-05-12 12:03:30');
+INSERT INTO study_infos(user_id, subject_id, study_time, date_time) VALUES(1, 3, 4300, '2020-05-12 16:12:30');
+INSERT INTO study_infos(user_id, subject_id, study_time, date_time) VALUES(1, 2, 300, '2020-05-12 19:12:30');
+INSERT INTO study_infos(user_id, subject_id, study_time, date_time) VALUES(1, 1, 10, '2020-05-12 23:40:00');
