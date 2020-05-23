4bd29b5ce14d4e8089fc501c67f9e7ae999f4d4e
suntorybikkle <suntorybikkle77@gmail.com>
2020-05-23 20:43:50 +0900

    fix: main関数の修正

--- main.go
+++ main.go
@@ -1,114 +1,26 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
-	"test/domain/model"
-	"test/interfaces"
-	"time"
+	"otter/pkg/infrastructure/postgresql"
+	"otter/pkg/infrastructure/webservice"
+	"otter/pkg/interfaces/controller"
+	"otter/pkg/interfaces/gateway"
+	"otter/pkg/usecases/interactor"
 )
 
-type StudyReportJson struct {
-	UserId     int             `json:"userId"`
-	UserName   string          `json:"userName"`
-	StudyInfos []StudyInfoJson `json:"studyInfos"`
-}
-
-type StudyInfoJson struct {
-	Id        int    `json:"studyId"`
-	SubjectId int    `json:"subId"`
-	StudyTime int    `json:"studyTime"`
-	DateTime  string `json:"dateTime"`
-}
-
-func ReportResponseAdapter(studyInfos []model.StudyInfo) (studyReportJson StudyReportJson) {
-	var studyInfosJson []StudyInfoJson
-	for _, studyInfo := range studyInfos {
-		studyInfosJson = append(studyInfosJson, StudyInfoJson{
-			Id:        studyInfo.Id,
-			SubjectId: studyInfo.SubjectId,
-			StudyTime: studyInfo.StudyTime,
-			DateTime:  studyInfo.DateTime.Format("2006-01-02 15:04:05"),
-		})
-	}
-	studyReportJson = StudyReportJson{UserId: 1, UserName: "ktguy", StudyInfos: studyInfosJson}
-	return
-}
-
-type StudyPostJson struct {
-	UserId    int    `json:"userId"`
-	SubjectId int    `json:"subId"`
-	StudyTime int    `json:"studyTime"`
-	DateTime  string `json:"dateTime"`
-}
-
-func StudyPostRequestAdapter(studyPostJson StudyPostJson) (studyInfo model.StudyInfo) {
-	studyDate, err := time.Parse("2006-01-02 15:04:05", studyPostJson.DateTime)
-	if err != nil {
-		log.Println(err)
-	}
-	studyInfo = model.StudyInfo{
-		UserId:    1,
-		SubjectId: studyPostJson.SubjectId,
-		StudyTime: studyPostJson.StudyTime,
-		DateTime:  studyDate,
-	}
-	return
-}
+var studyInfoController controller.StudyInfoController
 
 func main() {
+	dbHandler := postgresql.NewPsqlHandler("user=ldb dbname=ldb password=ldb sslmode=disable")
+	studyInfoRepository := gateway.NewStudyInfoRepository(dbHandler)
+	studyInfoInteractor := interactor.NewStudyInfoInteractor(studyInfoRepository)
+	studyInfoController := controller.NewStudyInfoController(studyInfoInteractor)
+	webserviceHandler := webservice.NewWebserviceHandler(studyInfoController)
+
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/record/", handleRequest)
-	server.ListenAndServe()
-}
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	var err error
-	switch r.Method {
-	case "GET":
-		err = handleGet(w, r)
-	case "POST":
-		err = handlePost(w, r)
-	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
-	studyInfos, err := interfaces.GetAllStudyInfo(1)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	studyReportJson := ReportResponseAdapter(studyInfos)
-	output, _ := json.MarshalIndent(&studyReportJson, "", "\t\t")
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Write(output)
-	return
-}
-
-func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
-
-	len := r.ContentLength
-	body := make([]byte, len)
-	r.Body.Read(body)
-
-	var studyPostJson StudyPostJson
-	json.Unmarshal(body, &studyPostJson)
-
-	studyInfo := StudyPostRequestAdapter(studyPostJson)
-	// studyInfo.Create()
-	interfaces.Create(studyInfo)
-
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	w.WriteHeader(200)
-	return
+	http.HandleFunc("/record/", webserviceHandler.HandleRequest)
+	_ = server.ListenAndServe()
 }
